d054bfc8f4d21bb42ca85fc90f59c9793f4bcf68
Tommy Tran <tranttommy95@gmail.com>
2019-05-31 14:03:05 -0700

    removed parameter

--- src/actions/thumbnailActions.js
+++ src/actions/thumbnailActions.js
@@ -1,7 +1,7 @@
 import { fetchPokemon } from '../services/pokemonApi';
 
 export const RETRIEVE_POKEMONS = 'this really doesn\t matter much does it';
-export const retrievePokemons = (input) => ({
+export const retrievePokemons = () => ({
   type: RETRIEVE_POKEMONS,
-  payload: fetchPokemon(input).then(json => json.results)
+  payload: fetchPokemon().then(json => json.results)
 });
