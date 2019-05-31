2fb04ca7e3ef9f058c15f634821837f91d711fef
Tommy Tran <tranttommy95@gmail.com>
2019-05-31 09:41:12 -0700

    added another test

--- src/utilities/utilities.test.js
+++ src/utilities/utilities.test.js
@@ -1,7 +1,11 @@
 import './utilities';
 
-describe('capitalize function', () => {
+describe('capitalize method', () => {
   it('capitalizes the first letter in a string', () => {
     expect('hello'.capitalize()).toBe('Hello');
+  });
+
+  it('capitalizes the first letter in a string', () => {
+    expect('hello world'.capitalize()).toBe('Hello world');
   });
 });
