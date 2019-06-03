f6c3fd0c92d9d059229795418d7551514654edf1
Tommy Tran <tranttommy95@gmail.com>
2019-06-03 09:20:21 -0700

    made switch case for adding comment

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -1,4 +1,4 @@
-import { DELETE_COMMENT } from '../actions/commentActions';
+import { DELETE_COMMENT, ADD_COMMENT } from '../actions/commentActions';
 
 const initialState = {
   '5cef3501ef6005a77cd4fd17': ['cool', 'rad']
@@ -8,6 +8,8 @@ export default function commentReducer(state = initialState, action) {
   switch(action.type) {
     case DELETE_COMMENT:
       return { ...state, [action.payload.id]: state[action.payload.id].filter((comment, i) => i !== action.payload.index) };
+    case ADD_COMMENT:
+      return { ...state, [action.payload.id]: [...state[action.payload.id], action.payload.comment] };
     default:
       return state;
   }
