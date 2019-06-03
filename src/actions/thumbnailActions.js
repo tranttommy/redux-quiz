398ce3a9d2fa278258a817ab022ce56da12514fd
Tommy Tran <tranttommy95@gmail.com>
2019-06-02 22:42:49 -0700

    refactored thumbnailActions

--- src/actions/thumbnailActions.js
+++ src/actions/thumbnailActions.js
@@ -1,7 +1,9 @@
 import { fetchPokemon } from '../services/pokemonApi';
 
-export const RETRIEVE_POKEMONS = 'this really doesn\t matter much does it';
-export const retrievePokemons = () => ({
-  type: RETRIEVE_POKEMONS,
-  payload: fetchPokemon().then(json => json.results)
+export const RETRIEVE_THUMBNAILS = 'RETRIEVE_THUMBNAILS';
+export const retrieveThumbnails = () => ({
+  type: RETRIEVE_THUMBNAILS,
+  payload: fetchPokemon()
+    .then(json => json.results)
+    .then(results => results.map(({ _id, id, pokemon, url_image }) => ({ _id, id, pokemon, url_image }))) 
 });
