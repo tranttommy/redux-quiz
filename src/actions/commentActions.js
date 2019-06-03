5d386dc7d9e345ce10dcb28b72b3c3b9baf4802d
Tommy Tran <tranttommy95@gmail.com>
2019-06-03 09:20:08 -0700

    made action for adding comment

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -3,3 +3,9 @@ export const deleteComment = (index, id) => ({
   type: DELETE_COMMENT,
   payload: { index, id }
 });
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const addComment = (comment, id) => ({
+  type: ADD_COMMENT,
+  payload: { comment, id }
+});
