94d06b17d55be536f977e66e4b9f21599d9861d1
Tommy Tran <tranttommy95@gmail.com>
2019-06-03 11:22:59 -0700

    refactored to thunk

--- src/actions/detailActions.js
+++ src/actions/detailActions.js
@@ -1,10 +1,11 @@
 import { fetchPokemon } from '../services/pokemonApi';
 
 export const RETRIEVE_DETAILS = 'RETRIEVE_DETAILS';
-export const retrieveDetails = id => ({
-  type: {
-    type: RETRIEVE_DETAILS,
-    id
-  },
-  payload: fetchPokemon({ id }),
-});
+export const retrieveDetails = id => dispatch =>
+  fetchPokemon({ id })
+    .then(details => {
+      dispatch({
+        type: RETRIEVE_DETAILS,
+        payload: details
+      });
+    });
