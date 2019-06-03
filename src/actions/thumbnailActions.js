6fd40f36bbabd0e5bd4f039545c507d65dba0214
Tommy Tran <tranttommy95@gmail.com>
2019-06-03 10:49:32 -0700

    made updatePage action

--- src/actions/thumbnailActions.js
+++ src/actions/thumbnailActions.js
@@ -1,9 +1,15 @@
 import { fetchPokemon } from '../services/pokemonApi';
 
 export const RETRIEVE_THUMBNAILS = 'RETRIEVE_THUMBNAILS';
-export const retrieveThumbnails = () => ({
+export const retrieveThumbnails = (query) => ({
   type: RETRIEVE_THUMBNAILS,
-  payload: fetchPokemon()
+  payload: fetchPokemon(query)
     .then(json => json.results)
     .then(results => results.map(({ _id, id, pokemon, url_image }) => ({ _id, id, pokemon, url_image }))) 
+});
+
+export const UPDATE_PAGE = 'UPDATE_PAGE';
+export const updatePage = newPage => ({
+  type: UPDATE_PAGE,
+  payload: newPage
 });
--- src/reducers/thumbnailReducer.js
+++ src/reducers/thumbnailReducer.js
@@ -1,11 +1,15 @@
-import { RETRIEVE_THUMBNAILS } from '../actions/thumbnailActions';
+import { RETRIEVE_THUMBNAILS, UPDATE_PAGE } from '../actions/thumbnailActions';
 
-const initialState = [];
+const initialState = {
+  currentPage: 1,
+};
 
 export default function thumbnailReducer(state = initialState, action) {
   switch(action.type) {
     case RETRIEVE_THUMBNAILS:
-      return action.payload;
+      return { ...state, results: action.payload };
+    case UPDATE_PAGE:
+      return { ...state, currentPage: action.payload };
     default:
       return state;
   }
