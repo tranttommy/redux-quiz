53f22890b90e5c73975b786e320fab7513ed11a3
Tommy Tran <tranttommy95@gmail.com>
2019-06-02 23:20:59 -0700

    hooked up commentReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import thumbnails from './thumbnailReducer';
 import details from './detailReducer';
+import comments from './commentReducer';
 
 export default combineReducers({
   thumbnails,
-  details
+  details,
+  comments
 });
