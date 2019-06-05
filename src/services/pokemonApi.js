0bf527b52c92a2994175f980aaf513dddc3bfde8
Tommy Tran <tranttommy95@gmail.com>
2019-06-04 19:41:07 -0700

    bug fix

--- src/services/pokemonApi.js
+++ src/services/pokemonApi.js
@@ -1,7 +1,7 @@
 export function fetchPokemon({ search = '', page = 1, id = '' } = {}) {
   let endpoint = `?page=${page}`;
   if(id) endpoint = id;
-  if(search) endpoint = `?pokemon=${search}&page=${page}`;
+  if(search) endpoint = `?pokemon=${search}&page=1`;
   return fetch(`https://alchemy-pokedex.herokuapp.com/api/pokedex/${endpoint}`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
