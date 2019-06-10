4172136a2287780f4c36a9cbd4761851bbe4b42c
Tommy Tran <tranttommy95@gmail.com>
2019-06-10 15:17:51 -0700

    did styling

--- src/components/thumbnails/Thumbnail.js
+++ src/components/thumbnails/Thumbnail.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
 import '../../utilities/utilities';
 
 export default function Thumbnail({ pokemon, url_image, _id }) {
+  const H1 = styled.h1`
+    color: crimson;
+  `;
+
   return (
-    <Link to={`/${_id}`}>
-      <h1>{pokemon.capitalize()}</h1>
+    <Link to={`/${_id}`} style={{ 'text-decoration': 'none' }}>
+      <H1>{pokemon.capitalize()}</H1>
       <img src={url_image} />
     </Link>
   );
--- src/components/thumbnails/Thumbnails.js
+++ src/components/thumbnails/Thumbnails.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Thumbnail from './Thumbnail';
+import styled from 'styled-components';
 
 export default function Thumbnails({ pokemons }) {
+  const Li = styled.li`
+    width: 250px;
+    list-style: none;
+    text-align: center;
+    border: 2px solid palegoldenrod;
+    box-shadow: 5px 10px 5px goldenrod;
+    margin: 20px;
+    & img {
+      width: 100%;
+    }
+  `;
+
   const pokemonList = pokemons.sort((a, b,) => a.id - b.id).map(({ pokemon, url_image, _id }) => (
-    <li key={_id}>
+    <Li key={_id}>
       <Thumbnail pokemon={pokemon} url_image={url_image} _id={_id} />
-    </li>
+    </Li>
   ));
   
+  const Ul = styled.ul`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+  `;
+
   return (
-    <ul>
+    <Ul>
       {pokemonList}
-    </ul>
+    </Ul>
   );
 }
 
